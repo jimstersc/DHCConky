ad8af41512519e2e1ffaa07601acb9fee3256113
jimstersc <jim.lathan@gmail.com>
2023-06-29 19:36:40 -0700

    Add files via upload

---
+++ dhcconkyinstall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install Conky-all
+sudo apt update
+sudo apt install conky-all -y
+sudp apt --fix-broken install
+sudo apt install conky-all -y
+
+# Extract conky.tar.xz
+tar -xf conky.tar
+
+# Change directory to conky
+cd conky
+
+# Copy conky.conf and conky.sh to /etc/conky/
+cp conky.conf /etc/conky/
+cp conky.sh /etc/conky/
+
+# Copy conky.desktop to /etc/xdg/autostart/
+cp conky.desktop /etc/xdg/autostart/
